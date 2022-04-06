9d716544c1f1695fe96e61a42335787ef8c8cdc8
Sandro Williner <sandro.williner@stud.hslu.ch>
2022-04-06 11:22:18 +0200

    Initial commit for testing hello world

---
+++ toradex_module/README.md
@@ -0,0 +1,2 @@
+# Node-red-test
+It's just a repo to test my own created first nodes for node-red
---
+++ toradex_module/app.js
@@ -0,0 +1,12 @@
+module.export = function(RED){
+    function helloWorld(config){
+        RED.nodes.createNode(this,config);
+        var context = this.context();
+        var node = this;
+        this.on('input', function(msg){
+            var outMsg = {payload: "HelloWorld"};
+            node.send(outMsg);
+        });
+    }
+    RED.nodes.registerType("Hello World",helloWorld);
+};

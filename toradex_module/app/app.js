f27ea077ea051d283658285353f7327b4a89729e
Sandro Williner <sandro.williner@stud.hslu.ch>
2022-05-25 19:42:42 +0200

    change hello world to gpio set for toradex

--- toradex_module/app/app.js
+++ toradex_module/app/app.js
@@ -11,6 +11,8 @@
     }
     RED.nodes.registerType("Hello World",helloWorld);
 };*/
+
+/*
 module.exports = function(RED){
     var GPIO = [
         ['PWR', 'out'],
@@ -65,3 +67,63 @@ module.exports = function(RED){
     }
     RED.nodes.registerType("Hello World",helloWorld);
 };
+
+*/
+
+
+module.exports = function(RED){
+    var GPIO = [
+        ['PWR', 'out'],
+        ['PWR', 'in'],
+        [6 ,9],
+        ['PWR', '0'],
+        [6, 8],
+        ['GND', 'GND'],
+        [3, 14],
+        [3, 2],
+        ['GND', 'GND'],
+        [3, 3],
+        [5, 22],
+        [0, 2],
+        [3, 19],
+        ['GND', 'GND'],
+        [4, 1],
+        [4, 11],
+        ['PWR', 'out'],
+        [3, 17],
+        [3, 9],
+        ['GND', 'GND'],
+        [3, 8],
+        [3, 16],
+        [3, 10],
+        [3, 23],
+        ['GND', 'GND'],
+        [3, 22],
+        [0, 7],
+        [0, 6],
+        [3, 13],
+        ['GND', 'GND'],
+        [3, 12],
+        [0, 9],
+        [0, 10],
+        ['GND', 'GND'],
+        [0, 11],
+        [5, 21],
+        [3, 18],
+        [3, 15],
+        ['GND', 'GND'],
+        [4, 0]
+    ];
+    function gpioSet(config){
+        RED.nodes.createNode(this,config);
+        this.pinNumber = config.pinNumber;
+        this.numberOnOff = config.NumberOnOff
+        var node = this;
+        this.on('input', function(msg){
+            msg.payload = "gpioset gpiochip" + GPIO[node.pinNumber][0] +" " + GPIO[node.pinNumber][1] +"=" + node.numberOnOff;
+            node.send(msg)
+        });
+    }
+    RED.nodes.registerType("GPIO Set",gpioSet);
+};
+

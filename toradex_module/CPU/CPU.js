50f68a77a9e7a08a7db65afa9c6af6e9e57c77b2
Sandro Williner <sandro.williner@stud.hslu.ch>
2022-06-06 06:47:41 +0200

    add exec function

--- toradex_module/CPU/CPU.js
+++ toradex_module/CPU/CPU.js
@@ -1,10 +1,21 @@
+const {exec} = require('child_process');
 module.exports = function(RED){
     
     function cpuTemp(config){
         RED.nodes.createNode(this, config);
         var node = this;
         this.on('input', function(msg){
-            msg.payload = "cat /sys/devices/virtual/thermal/thermal_zone0/temp";
+            //msg.payload = "cat /sys/devices/virtual/thermal/thermal_zone0/temp";
+            exec('cat /sys/devices/virtual/thermal/thermal_zone0/temp',(error, stdout, stderr) => {
+                if (error){
+                    msg.payload = "error:"+ error.message;
+                }
+                 else if(stderr){
+                    msg.payload = "stderr" + stderr;
+                }else {
+                    msg.payload = stdout;
+                }
+            });
             node.send(msg)
         });
     }
